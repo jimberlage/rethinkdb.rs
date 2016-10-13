56124eeb0869c60ea0664090340b5b1a89017b14
Jim Berlage <james.berlage@gmail.com>
2016-10-13 13:50:12 -0700

    feature(documentation): Adds documentation for a few things
    This gives an overview of the project and some handy links to get
    started.

--- src/reql/tree.rs
+++ src/reql/tree.rs
@@ -1,7 +1,15 @@
+use protobuf::ProtobufEnum;
 use rustc_serialize::json::{Json, ToJson};
-use super::ql2::Term_TermType;
+use super::super::ql2::Term_TermType;
 
-// Each ReQL query/command is represented as a tree.
+/// Each ReQL query/command is represented as a tree, which is serialized to a JSON array like:
+///
+/// ```
+/// [<term>, <tree0>, <tree1>, ...]
+/// ```
+///
+/// An array is a new query, unless it is prefixed with the MAKE_ARRAY term, in which case what
+/// follows is a plain JSON array.  Plain JSON elements are represented with the Tree::Datum type.
 pub enum Tree {
     Query {
         head: Term_TermType,
@@ -10,25 +18,27 @@ pub enum Tree {
     Datum(Json)
 }
 
-// Each tree can be represented in JSON by an array.
 impl ToJson for Tree {
     fn to_json(&self) -> Json {
         match self {
             &Tree::Query {
-                head: head,
-                tail: tail,
+                head,
+                ref tail,
             } => {
-                let array = vec![head.to_json()];
+                // Return a JSON array, with the term first (as JSON).  Then, recursively convert
+                // any other parts of the query to JSON.
+                let mut array = vec![head.value().to_json()];
                 array.extend(tail.iter().map(|tree| tree.to_json()).collect::<Vec<Json>>());
 
                 Json::Array(array)
             },
             // We need to transform JSON arrays to the special RethinkDB representation, which uses
             // the MAKE_ARRAY term.
-            &Tree::Datum(Json::Array(array)) => {
-                Json::Array(vec![Term_TermType::MAKE_ARRAY.to_json(), Json::Array(array.iter().cloned())])
-            },
-            &Tree::Datum(json) => json.clone(),
+            &Tree::Datum(Json::Array(ref array)) => Json::Array(vec![
+                Term_TermType::MAKE_ARRAY.value().to_json(),
+                Json::Array(array.iter().cloned().collect::<Vec<Json>>()),
+            ]),
+            &Tree::Datum(ref json) => json.clone(),
         }
     }
 }
